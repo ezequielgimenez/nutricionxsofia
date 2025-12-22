cc0638346a2f4509b0661285f3e4bfbe049091b7
Ezequiel <ezequielezequiel9@gmail.com>
2025-12-21 21:26:48 -0300

    edit bg loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#1F2937]">
+    <div className="flex min-h-screen items-center justify-center bg-[#EEEFE7]">
       <div className="flex flex-col items-center gap-1 animate-pulse">
         <h2 className="font-black text-[40px] text-[#DEDFD8] leading-none">
           Nutrición
