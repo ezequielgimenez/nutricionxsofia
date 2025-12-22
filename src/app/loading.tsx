1f8c3cc783de5ba34706a506d7f88c153b8a98f7
Ezequiel <ezequielezequiel9@gmail.com>
2025-12-22 01:07:45 -0300

    edit schema

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,10 +2,10 @@ export default function Loading() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#EEEFE7]">
       <div className="flex flex-col items-center gap-1 animate-pulse">
-        <h2 className="font-black text-[70px] text-[#DEDFD8] leading-none">
+        <h2 className="font-black text-[70px] text-[##a6a6a6] leading-none">
           Nutrición
         </h2>
-        <p className="font-nunito text-[#DEDFD8] text-[41px] font-extrabold tracking-[0.75em] leading-none">
+        <p className="font-nunito text-[##a6a6a6] text-[20px] font-extrabold tracking-[0.75em] leading-none">
           X SOFÍA
         </p>
       </div>
