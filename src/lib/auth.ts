2c3de08213fd8843f41d9c883b72a47a0d81df4b
Ezequiel <ezequielezequiel9@gmail.com>
2025-12-22 01:27:55 -0300

    edit auth for email.js

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,14 +9,14 @@ export const authOptions: NextAuthOptions = {
   providers: [
     EmailProvider({
       server: {
-        host: "smtp.resend.com",
-        port: 587,
+        host: "smtp.mailersend.net",
+        port: 587, // también puede ser 2525
         auth: {
-          user: "resend",
-          pass: process.env.RESEND_API_KEY,
+          user: process.env.MAILERSEND_USER, // pones MS_BY8Myv@test-zxk54v8n726ljy6v.mlsender.net
+          pass: process.env.MAILERSEND_PASS, // pones mssp.s1FZ6Pg.z86org8xem04ew13.MQOlC2i
         },
       },
-      from: process.env.EMAIL_FROM,
+      from: process.env.EMAIL_FROM, // debe ser un correo verificado en MailerSend
       maxAge: 10 * 60,
     }),
   ],
