4b6a687ff37cdbd6c80de2580e98813d21adb220
Ezequiel <ezequielezequiel9@gmail.com>
2025-12-21 21:09:52 -0300

    add loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#1F2937]">
+      <div className="flex flex-col items-center gap-1 animate-pulse">
+        <h2 className="font-black text-[40px] text-[#DEDFD8] leading-none">
+          Nutrición
+        </h2>
+        <p className="font-nunito text-[#DEDFD8] text-[12px] font-extrabold tracking-[0.75em] leading-none">
+          X SOFÍA
+        </p>
+      </div>
+    </div>
+  );
+}
